38d0db966ed8da456339bab2a99b88e34da093bf
Loonibha <loonibha.tamrakar@gmail.com>
2025-07-17 11:17:19 +0545

    graph

--- NM.py
+++ NM.py
@@ -18,3 +18,11 @@ x_result,y_result = backward_euler(0,1,1,20,lambda x, y: (y**2 - x**2)/(y**2 + x
 print('x \t y')
 for x,y in zip(x_result, y_result):
     print(f'{x:.2f} \t {y:.2f}')
+
+from matplotlib import pyplot as plt
+plt.plot(x_result, y_result, label='Backward Euler Method')
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
